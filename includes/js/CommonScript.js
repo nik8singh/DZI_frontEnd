823b918662cf1d614aea639c50654ba124ab8e06
Nikhil Singh <nikhil.singh@partner.vermont.gov>
2018-09-05 15:42:00 -0400

    Sign in module

--- includes/js/CommonScript.js
+++ includes/js/CommonScript.js
@@ -31,3 +31,45 @@ $( "body" ).on('click', '.feature', function(){
     window.location.href="productPage.html";
 });
 
+function signOut() {
+    $('#signInNav').css("display","block");
+
+    var auth2 = gapi.auth2.getAuthInstance();
+
+    auth2.signOut().then(function () {
+        console.log('User signed out.');
+    });
+
+
+    var $alert = $("#alert");
+    $alert.text("Sign out successful");
+    setTimeout(function() {
+        $alert.text("");
+     }, 3000);
+
+    window.location.reload(true);
+}
+
+function onSignIn(googleUser) {
+    // Useful data for your client-side scripts:
+    var profile = googleUser.getBasicProfile();
+    console.log("ID: " + profile.getId()); // Don't send this directly to your server!
+    console.log('Full Name: ' + profile.getName());
+    console.log('Given Name: ' + profile.getGivenName());
+    console.log('Family Name: ' + profile.getFamilyName());
+    console.log("Image URL: " + profile.getImageUrl());
+    console.log("Email: " + profile.getEmail());
+
+    // The ID token you need to pass to your backend:
+    var id_token = googleUser.getAuthResponse().id_token;
+    console.log("ID Token: " + id_token);
+
+    $('#signupModal').modal('toggle');
+    $('#signInNav').css("display","none");
+    var $alert = $("#alert");
+    $alert.text("Sign In successful");
+
+    setTimeout(function() {
+        $alert.text("");
+    }, 3000);
+}
--- includes/js/indexScript.js
+++ includes/js/indexScript.js
@@ -1,15 +1,1 @@
-/**
- * Created by Nikhil Singh on 1/16/2016.
- */
 
-    $('.carousel').carousel({
-        interval: 3000
-    });
-
-$('body').on({
-    click: function() {
-        $('html, body').animate({
-            scrollTop: $("#bigCallout").offset().top
-        }, 1000);
-    }
-},'#signInNav');
